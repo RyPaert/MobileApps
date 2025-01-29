f08c9ca882ce69848ada977b3ca6da3be38fe970
Opilane TTHK <opilane@tthk.ee>
2025-01-29 11:38:33 +0200

    component model added

---
+++ ScientificCalculator/ViewModels/CalculatorPageViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using CommunityToolkit.Mvvm.ComponentModel;
+using System.Text;
+
+namespace ViewModels
+{
+    [INotifyPropertyChanged]
+    internal partial class CalculatorPageViewModel
+    {
+    }
+}
