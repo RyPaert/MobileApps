e910f355ed88482c9c490ae10d78a0b6f6acc128
Opilane TTHK <opilane@tthk.ee>
2025-02-19 11:11:36 +0200

    sandra vaata siia

---
+++ MauiCRUD/MauiCRUD/Data/DatabaseContext.cs
@@ -0,0 +1,38 @@
+﻿using SQLite;
+
+namespace Data
+{
+    public class DatabaseContext
+    {
+        private const string DbName = "CRUDdb3";
+        private static string DbPath => Path.Combine(".", DbName);
+
+        private SQLiteAsyncConnection _connection;
+
+        private SQLiteAsyncConnection Database => (_connection ??= new SQLiteAsyncConnection(DbPath,
+            SQLiteOpenFlags.Create | SQLiteOpenFlags.ReadWrite | SQLiteOpenFlags.SharedCache));
+
+        public async Task<IEnumerable<TTable>> GetAllAsync<TTable>() where TTable : class, new()
+        {
+            var table = await GetTableAsync<TTable>();
+            return await table.ToListAsync();
+        }
+
+        public async Task<AsyncTableQuery<TTable>> GetTableAsync<TTable>() where TTable : class, new()
+        {
+            await CreateTableIfNotExists<TTable>();
+            return Database.Table<TTable>();
+        }
+
+        public async Task CreateTableIfNotExists<TTable>() where TTable : class, new()
+        {
+            await Database.CreateTableAsync<TTable>();
+        }
+
+        public async Task<TResult> Execute<TTable, TResult>(Func<Task<TResult>> action) where TTable : class, new()
+        {
+            await CreateTableIfNotExists<TTable>();
+            return await action();
+        }
+    }
+}
