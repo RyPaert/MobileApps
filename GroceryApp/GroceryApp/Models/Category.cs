852fcd6f57c741ebf8ab076f0ff67a5fcd563717
Opilane TTHK <opilane@tthk.ee>
2025-03-03 13:08:10 +0200

    Part 1 Complete

--- GroceryApp/GroceryApp/Models/Category.cs
+++ GroceryApp/GroceryApp/Models/Category.cs
@@ -13,39 +13,4 @@ namespace Models
         public short? ParentId { get; set; }
         public bool IsMainCategory => ParentId == 0;
     }
-
-    public class Product
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public string Image { get; set; }
-        public decimal Price { get; set; }
-        public short CategoryId { get; set; }
-        public string CategoryName { get; set; }
-    }
-    public partial class CartItem : ObservableObject
-    {
-        public Guid Id { get; set; }
-        public int ProductId { get; set; }
-        public string ProductName { get; set; }
-        public decimal Price { get; set; }
-
-        [ObservableProperty, NotifyPropertyChangedFor(nameof(Amount))]
-        private int _quantity;
-        public decimal Amount => Price * Quantity;
-    }
-    public class Order
-    {
-        public long Id { get; set; }
-        public IEnumerable<CartItem> Items { get; set; } = Enumerable.Empty<CartItem>();
-        public DateTime Date { get; set; } = DateTime.Now;
-        public decimal TotalAmount => Items.Sum(i => i.Amount);
-    }
-    public enum OrderStatus
-    {
-        Placed = 0,
-        Confirmed = 1,
-        Delivered = 2,
-        Cancelled = 3
-    }
 }
---
+++ GroceryApp/GroceryApp/Models/Order.cs
@@ -0,0 +1,24 @@
+﻿using GroceryApp.Models.Enumerations;
+
+namespace Models
+{
+    public class Order
+    {
+        public long Id { get; set; }
+        public IEnumerable<CartItem> Items { get; set; } = Enumerable.Empty<CartItem>();
+        public DateTime Date { get; set; } = DateTime.Now;
+        public decimal TotalAmount => Items.Sum(i => i.Amount);
+        public OrderStatus Status { get; set; }
+
+        public Color Color => _orderStatusColorMap[Status];
+
+        private static readonly IReadOnlyDictionary<OrderStatus, Color> _orderStatusColorMap =
+            new Dictionary<OrderStatus, Color>
+            {
+                [OrderStatus.Placed] = Colors.Yellow,
+                [OrderStatus.Placed] = Colors.Blue,
+                [OrderStatus.Placed] = Colors.Green,
+                [OrderStatus.Placed] = Colors.Red
+            };
+    }
+}
